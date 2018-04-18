e1ed0fc5dbe9ee39bd7ae6e45a36b0972a6064a5
TNTdesigner <TNTdesigner@users.noreply.github.com>
2018-04-18 11:55:29 +0200

    driver scherm

--- programma/DigiSketch.asm
+++ programma/DigiSketch.asm
@@ -1,6 +1,3 @@
-clrL    equ     15h                 ;register voor het legen van het het scherm
-clrH    equ     16h                 ;register voor het legen van het het scherm
-
 org 0000h
 ljmp   main
 
@@ -31,6 +28,7 @@ main:
 
 lus0:
     mov     dptr,#startscherm       ;begin afbeelding in pointer steken
+;    mov     dptr,#rogue       ;begin afbeelding in pointer steken
 lus:
     clr     a
     movc    a,@a+dptr               ;get byte
@@ -55,48 +53,8 @@ stop:
 inits:
     mov     port_page,#000h         ;selecteer poort page 0
     lcall   initGLCD                ;initializatie grafisch LCD scherm, MOET EERST GEBEUREN
-    lcall   clearScreen
+;    lcall   clearScreen
 ret
-
-;------------------------------------------------------------------
-;Het scherm leeg maken
-;------------------------------------------------------------------
-clearScreen:
-    push    acc
-    push    psw
-    mov     clrL,#ffh               ;255 keer doolopen
-    mov     clrH,#0fh               ;16 keer doorlopen           
-    ;Mode set
-    mov     COMANDREG,#10000000b    ;OR mode    
-    lcall   comandWrite
-    ;Display mode
-    mov     COMANDREG,#10011100b    ;gafisch en tekst aan zetten
-    lcall   comandWrite
-    ;auto write mode
-    mov     COMANDREG,#b0h
-    lcall   comandWrite
-lus2400:
-    mov     DATAREG,#000h
-    lcall   dataAutoWrite
-    djnz    clrL,lus2400
-    mov     clrL,#ffh
-    djnz    clrH,lus2400
-    ;data auto read af zetten
-    mov     COMANDREG,#b2h
-    lcall   comandWrite
-
-    ;adres data pointer resetten
-    mov     DATAREG,#00h            ;lower address
-    lcall   dataWrite
-    mov     DATAREG,#00h
-    lcall   dataWrite
-    mov     COMANDREG,#24h
-    lcall   comandWrite
-    pop     acc
-    pop     psw
-ret
-
-
 
 
 ;include filles
