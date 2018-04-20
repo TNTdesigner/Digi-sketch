3836413f3ceffdb00d93f879f63ef2747324d8b8
TNTdesigner <TNTdesigner@users.noreply.github.com>
2018-04-20 12:00:45 +0200

    lezen van encoders

--- programma/DigiSketch.asm
+++ programma/DigiSketch.asm
@@ -1,6 +1,21 @@
 org 0000h
 ljmp   main
 
+org 000bh
+    push    acc
+    push    psw
+    mov     a,p2_data
+    cjne    a,ROTARYTEMP,verzet
+    ljmp    stopRotary
+verzet:
+    mov     r0,a
+    lcall   debug
+    mov     ROTARYTEMP,a
+stopRotary:
+    pop     psw
+    pop     acc
+reti
+
 ;------------------------------------------------------------------
 ;Hoofd programma
 ;------------------------------------------------------------------
@@ -9,41 +24,41 @@ main:
     lcall   inits
 
     ;Mode set
-    mov     COMANDREG,#10000000b
-    lcall   comandWrite
+;    mov     COMANDREG,#10000000b
+;    lcall   comandWrite
     ;Display mode
-    mov     COMANDREG,#10011100b
-    lcall   comandWrite
+;    mov     COMANDREG,#10011100b
+;    lcall   comandWrite
 
     ;adres data pointer
-    mov     DATAREG,#00h            ;lower address
-    lcall   dataWrite
-    mov     DATAREG,#00h
-    lcall   dataWrite
-    mov     COMANDREG,#24h
-    lcall   comandWrite
+;    mov     DATAREG,#00h            ;lower address
+;    lcall   dataWrite
+;    mov     DATAREG,#00h
+;    lcall   dataWrite
+;    mov     COMANDREG,#24h
+;    lcall   comandWrite
     ;auto write mode
-    mov     COMANDREG,#b0h
-    lcall   comandWrite
+;    mov     COMANDREG,#b0h
+;    lcall   comandWrite
 
-lus0:
+;lus0:
 ;    mov     dptr,#startscherm       ;begin afbeelding in pointer steken
-    mov     dptr,#rogue       ;begin afbeelding in pointer steken
-lus:
-    clr     a
-    movc    a,@a+dptr               ;get byte
-    cjne    a,#10101010b,next
-    ljmp    stop
-next:    
-    mov     DATAREG,a
-    lcall   dataAutoWrite
-    inc     dptr
-    ljmp    lus
+;    mov     dptr,#rogue       ;begin afbeelding in pointer steken
+;lus:
+;    clr     a
+;    movc    a,@a+dptr               ;get byte
+;    cjne    a,#10101010b,next
+;    ljmp    stop
+;next:    
+;    mov     DATAREG,a
+;    lcall   dataAutoWrite
+;    inc     dptr
+;    ljmp    lus
     ;data auto read af zetten
-    mov     COMANDREG,#b2h
-    lcall   comandWrite
+;    mov     COMANDREG,#b2h
+;    lcall   comandWrite
 stop:
-    lcall   readRotary
+    
     ljmp    stop    
 
 
@@ -51,8 +66,10 @@ stop:
 ;Initializatie van alle componenten nodig in het hoofd programma
 ;------------------------------------------------------------------
 inits:
+    lcall   initsio                 ;voor debug
     mov     port_page,#000h         ;selecteer poort page 0
-    lcall   initGLCD                ;initializatie grafisch LCD scherm, MOET EERST GEBEUREN
+;    lcall   initGLCD                ;initializatie grafisch LCD scherm, MOET EERST GEBEUREN
+    lcall   initRotary
 ;    lcall   clearScreen
 ret
 
